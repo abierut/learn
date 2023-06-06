27d229bc2bd4c99d8f792eb69f90fa3c73439572
abierut <alexbierut@gmail.com>
2023-06-05 20:04:57 -0600

    Adding numpy to venv and using np functions

--- greet.py
+++ greet.py
@@ -5,3 +5,6 @@ for x in range (10):
 another_variable = [1,2,3,4,5,6]
 print (another_variable)
 
+import numpy as np
+another_variable = np.array(another_variable)
+print(another_variable.mean(),another_variable.std())
